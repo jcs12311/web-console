ab8bda583cd63313e1f83a0c2055511c5c8a5fd6
jcsoo <jcs12311@gmail.com>
2016-06-20 17:50:38 +0800

    make usage more easy

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 2.use like demo.html
 3.open http://127.0.0.1:3000 and try to log something on your website
 4.you will see your log result on http://127.0.0.1:3000
-
--- web-console.js
+++ web-console.js
@@ -1,36 +1,34 @@
-var WebConsole = function() {
-    var implement = function(host, params) {
-        if (!host) {
-            console.error('you must specfiy a host to connect');
-        }
-        var defaults = {
-            methods: ['log', 'error', 'warn', 'time', 'timeEnd'], // console methods
-        }
-        var p = params ? params : {};
-        for (var def in defaults) {
-            if (!p.hasOwnProperty(def)) {
-                p[def] = defaults[def];
-            }
-        }
-        var socket = io(host);
-
-        function bindConsoleWidthSocket(method) {
-            var oldMethod = console[method];
-            console[method] = function() {
-                socket.emit('echo', {
-                    type: method,
-                    args: arguments
-                })
-                oldMethod.apply(console, arguments);
-            }
-        }
-        var methods = p.methods;
-        methods.forEach(function(method) {
-            bindConsoleWidthSocket(method);
-        });
+var WebConsole = function(params) {
+    var defaults = {
+        methods: ['log', 'error', 'warn', 'time', 'timeEnd'], // console methods
     }
 
-    return {
-        implement: implement
+    var p = {};
+    for(var def in defaults) {
+         if (typeof params[def] === 'undefined') {
+            params[def] = defaults[def];
+        }
     }
+    this.params = params;
+    
+    if (!this.params.host) {
+        console.error('you must specfiy a host to connect');
+    }
+    
+    var socket = io(this.params.host);
+
+    function bindConsoleWidthSocket(method) {
+        var oldMethod = console[method];
+        console[method] = function() {
+            socket.emit('echo', {
+                type: method,
+                args: arguments
+            })
+            oldMethod.apply(console, arguments);
+        }
+    }
+    var methods = this.params.methods;
+    methods.forEach(function(method) {
+        bindConsoleWidthSocket(method);
+    });
 }
